715c694d5f73c0cb91cae0d2fe177f192f039e2a
Ketnez <dimabengardt@gmail.com>
2021-11-09 09:57:05 +0300

    Добавьте файлы проекта.

---
+++ Lib13/Practice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Lib13
+{
+    public class Practice
+    {
+
+        public static (string, string) CalculatingNumbers()
+        {
+            Random random = new Random();
+            int pastValue = -6;
+            int value = random.Next(-5, 5);
+
+
+            string numbers = string.Empty;
+            string results = string.Empty;
+
+            double x = 0;
+            while (pastValue != value)
+            {
+                if (value > 0)
+                {
+                    x = Math.Sqrt(value);
+
+                }
+                if (value < 0)
+                {
+                    x = value * value;
+
+                }
+                numbers += value + ",";
+                results += x + ",";
+
+
+
+                pastValue = value;
+                value = random.Next(-5, 5);
+
+
+            }
+            return (numbers, results);
+
+        }
+
+    }
+}
+
---
+++ Practice 1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib13;
+
+namespace Practice_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerationAndCalculating_Click(object sender, RoutedEventArgs e)
+        {
+            var results = Practice.CalculatingNumbers();
+            result.Text = results.ToString();
+            var numbers = Practice.CalculatingNumbers();
+            number.Text = numbers.ToString();
+
+
+        }
+
+        private void result_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void number_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
